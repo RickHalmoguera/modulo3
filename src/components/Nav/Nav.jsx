52d0613d4d779d09c9896bcbf1354657da173591
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-10-06 13:20:31 +0200

    nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ImageSearchIcon from '@mui/icons-material/ImageSearch';
+export const Nav =()=> {
+  return (
+    <>
+        <p>PG</p>
+        <img src={ImageSearchIcon} alt="" />
+
+    </>
+  )
+}
