86240c08f887b0d04e7f54171d2f17e7fa80b47e
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-10-11 10:37:51 +0200

    edit mode done!

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,20 +1,19 @@
-
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { getFavoritesData } from '../features/favoritesSlice/favoritesSlice'
 import { removeFavorite, updatePhotoFavoritesList } from '../features/favoritesSlice/favoritesSlice'
-import { getPhotoData, updatePhotoList } from "../features/search/searchSlice" 
+import { getPhotoData, updatePhotoList } from "../features/search/searchSlice"
 import EditIcon from '@mui/icons-material/Edit'
 import SaveIcon from '@mui/icons-material/Save'
 import TurnedInIcon from '@mui/icons-material/TurnedIn'
 import { useState } from 'react'
 
-export const Favorites= () => {
+export const Favorites = () => {
   const photos = useSelector(getPhotoData)
   const favorites = useSelector(getFavoritesData)
   const dispatch = useDispatch()
-  const [isEditModeOn, setIsEditModeOn] = useState(false)
 
+  const [editingPhotoId, setEditingPhotoId] = useState(null) 
   const [newDescription, setNewDescription] = useState("")
 
   const handleRemoveFavorite = (photo) => {
@@ -22,25 +21,24 @@ export const Favorites= () => {
     dispatch(removeFavorite(idToRemove))
 
     const updatedPhotosToShow = photos.map((item) =>
-    item.id === idToRemove ? { ...item, isFavorite: false } : item
+      item.id === idToRemove ? { ...item, isFavorite: false } : item
     )
     dispatch(updatePhotoList(updatedPhotosToShow))
   }
 
-  const handleEdit =(photo) => {
-    
-    const photoModify = photo.id 
+  const handleEdit = (photo) => {
+    const photoModify = photo.id
 
     const updatedPhotosToShow = favorites.map((item) =>
-    item.id === photoModify ? { ...item, description: newDescription } : item
+      item.id === photoModify ? { ...item, description: newDescription } : item
     )
-    
+
     dispatch(updatePhotoFavoritesList(updatedPhotosToShow))
-    setIsEditModeOn(false)
+    setEditingPhotoId(null) 
   }
 
-  const handleClick =(photo)=>{
-    setIsEditModeOn(true)
+  const handleClick = (photo) => {
+    setEditingPhotoId(photo.id) 
   }
 
   return (
@@ -53,9 +51,21 @@ export const Favorites= () => {
           <img src={photo.img} alt={photo.description} />
           <TurnedInIcon onClick={() => handleRemoveFavorite(photo)} />
           <div className="photo-details">
-           { isEditModeOn? <input type='text' placeholder={photo.description} onChange={(e)=> setNewDescription(e.currentTarget.value)} /> : <p>{photo.description}</p> }
-            <button >
-              {isEditModeOn ? <SaveIcon  onClick={()=> handleEdit(photo)}/> : <EditIcon onClick={()=> handleClick(photo)}/> }
+            {editingPhotoId === photo.id ? (
+              <input
+                type='text'
+                placeholder={photo.description}
+                onChange={(e) => setNewDescription(e.currentTarget.value)}
+              />
+            ) : (
+              <p>{photo.description}</p>
+            )}
+            <button>
+              {editingPhotoId === photo.id ? (
+                <SaveIcon onClick={() => handleEdit(photo)} />
+              ) : (
+                <EditIcon onClick={() => handleClick(photo)} />
+              )}
             </button>
             <p>Date: {photo.date}</p>
             <p>Dimensions: {photo.width}x{photo.height}</p>
