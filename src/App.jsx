2acede77fa35a531733bfed7784808a359f283df
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-10-12 12:08:42 +0200

    Nav mobile done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import { Root } from "./pages/Root"
 import { Search } from "./pages/Search"
 import { Favorites } from "./pages/Favorites"
+import './App.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const font = "'Outfit', sans-serif"
+const THEME = createTheme({
+  typography: {
+   "fontFamily": font,
+   "fontSize": 10,
+   "fontWeightLight": 100,
+   "fontWeightRegular": 400,
+   "fontWeightBold": 700
+  }
+}) 
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root/>}>
@@ -12,12 +26,13 @@ const router = createBrowserRouter(createRoutesFromElements(
 ))
 
 function App() {
-
-
   return (
-    <RouterProvider router ={router}>
-     <Root />
-    </RouterProvider>
+    <ThemeProvider theme ={THEME}>
+      <CssBaseline />
+      <RouterProvider router ={router}>
+      <Root />
+      </RouterProvider>
+    </ThemeProvider>
   )
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,17 +2,53 @@ import React from 'react'
 import ImageSearchIcon from '@mui/icons-material/ImageSearch';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { NavLink } from 'react-router-dom';
+
+
+import Box from '@mui/material/Box'
+import { Toolbar } from '@mui/material'
+import Typography from '@mui/material/Typography'
+
 export const Nav =()=> {
   return (
-    <>
-      <p>PG</p>
-      <NavLink to ="/">
-        <ImageSearchIcon/>
-      </NavLink>
-      <NavLink to ="/favorites">
-        <FavoriteIcon/>
-      </NavLink>
+    <Box 
+    sx={{ backgroundColor: '#161D2F',
+    display: 'flex',
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    padding: '10px 20px', }}>
+        <Typography 
+        variant='h2' 
+        color="#FC4747" 
+        fontWeight='fontWeightBold'>
+          PG
+        </Typography>
+        <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap:'1em',
+          flex: 1, 
+        }}>
+        <NavLink 
+        to ="/" 
+        style={({ isActive }) => ({
+          color: isActive ? '#fff' : '#545e6f',
+        })}>
+          <ImageSearchIcon 
+          sx={{fontSize:32}}
+          />
+        </NavLink>
+        <NavLink to ="/favorites"
+        style={({ isActive }) => ({
+          color: isActive ? '#fff' : '#545e6f',
+        })}>
+          <FavoriteIcon 
+          sx={{fontSize:32}}
+          />
+        </NavLink>
+        </Box>
       
-    </>
+    </Box>
   )
 }
